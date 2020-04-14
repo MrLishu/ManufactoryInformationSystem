7ad5317df8269bb783abfb95e17b6389949c1f2d
KANG-KANG <63388197+man-in@users.noreply.github.com>
2020-04-14 22:36:43 +0800

    kk

--- Test.cs
+++ Test.cs
@@ -21,5 +21,10 @@ namespace ManufactoryInformationSystem
         {
             textBox1.Text = "bye！！！！";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "bye！！！！";
+        }
     }
 }
